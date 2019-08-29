099b5e973257093c463a42e074a1ca75d64ab98c
WANGFENG <825528411@qq.com>
2019-08-29 20:16:11 +0800

    mysql建库语句

---
+++ thymeleaf_mybatis.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.5.40 : Database - how2java
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`how2java` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `how2java`;
+
+/*Table structure for table `category_` */
+
+DROP TABLE IF EXISTS `category_`;
+
+CREATE TABLE `category_` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+/*Data for the table `category_` */
+
+insert  into `category_`(`id`,`name`) values (1,'Java'),(2,'Python'),(3,'C#'),(4,'C++'),(6,'Ruby'),(7,'Scala'),(9,'Erlang'),(11,'C'),(13,'Go'),(14,'Erlang'),(15,'Ruby'),(16,'易语言'),(17,'cccc'),(18,'我爱你');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
